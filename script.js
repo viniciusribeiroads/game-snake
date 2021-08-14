b836a3a6411963e0a939eabc74778cf991d4ea8f
Vinicius Ribeiro <viniciusribeiro113@gmail.com>
2021-08-13 21:41:05 -0300

    Snake em JS
    Código comentado com fins de fixar o conteudo aprendido.

--- script.js
+++ script.js
@@ -1,10 +1,89 @@
-let canvas = document.getElementById("snake");
-let context = canvas.getContext('2d');
-let box = 32;
+windows.onload = function(){
+    
+    var stage = document.getElementById('stage');
+    var ctx = stage.getContext("2d");
+    document.addEventListener("keydown", keyPush);
+    setInterval(game, 85);
 
-function criarBG() {
-    context.fillStyle = "lightgreen";
-    context.fillRect(0,0, 16 * box, 16*box);
+    const vel = 1;
+
+    var vx = vy =0;
+    var px = 10;
+    var py = 14;
+
+    var tp = 30;
+    var qp = 20;
+    var ax=ay=15;
+
+    var trail = [];
+    tail = 5;
+
+    function game(){
+        px += vx;
+        py += vy;
+
+        if (px<0) {
+            px = qp - 1;
+        }
+        if (px > qp-1) {
+            px = 0;
+        }
+        if (py < 0){
+            py = qp-1;
+        }
+        if (py > qp -1){
+            py = 0;
+        }
+
+
+        ctx.fillStle = "green";
+        ctx.fillRect(0,0,stage.clientWidth, stage.height);
+
+        ctx.fillStyle = "red";
+        ctx.fillRect(ax*tp, ay*tp, to, tp);
+
+        ctx.fillStyle = "gray";
+        for (var i = 0; i < trail.length; i++){
+            ctx.fillRect(trail[i].x == px && trail[i].y*tp, tp-1, tp-1);
+                if (trail[i].x == px && trail[i].y == py)
+            {
+                vx = vy = 0;
+                tail = 5;
+            }
+            trail.unshift({ x:px, t:py})
+            while (trail.length > tail){
+                trail.shift();
+            }
+            if (ax==px && ay==py){
+                tail++;
+                ax = Math.floor(Math.random()*qp);
+                ay = Math.floor(Math.random()*qp);
+            }
+        
+        }
+
+        function keyPush(event){
+            switch (event.keycode) {
+                case 37:
+                    vx = -vel;
+                    vy = 0;
+                    break;
+                case 38:
+                    vx = 0;
+                    vy = -vel;
+                    break;
+                case 39:
+                    vx = vel;
+                    vy = 0;
+                    break;
+                case 40:
+                    vx = 0;
+                    vy = vel;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
 }
-
-criarBG()
